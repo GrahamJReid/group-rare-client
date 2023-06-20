2be6aeb64f0a49bd1c874793b3572e7d00a3ff8a
GrahamJReid <grahamreidtechnology@gmail.com>
2023-06-20 10:39:35 -0500

    post details page functional

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Button } from 'react-bootstrap';
@@ -14,7 +16,12 @@ function PostDetails() {
 
   return (
     <>
-      <h1>{post.title}</h1>
+      <h1> Title: {post.title}</h1>
+      <h3>Published: {post.publication_date}</h3>
+      <h2>Author: {post.rare_user_id.first_name} {post.rare_user_id.last_name}</h2>
+      <h4>Category: {post.category_id.label}</h4>
+      <img src={post.image_url} />
+      <p>{post.content}</p>
       <Button
         onClick={() => {
           router.push(`/comments/${id}`);
