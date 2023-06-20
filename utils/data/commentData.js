a8bccab6f9909b0ec2e3dcaa33539a22c8b1ea15
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-06-19 20:26:06 -0500

    commentData.js finished

---
+++ utils/data/commentData.js
@@ -0,0 +1,65 @@
+import { clientCredentials } from '../client';
+
+const createComment = (comment) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(comment),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getSingleComment = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getAllComments = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const updateComment = (comment) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments/${comment.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(comment),
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteComment = (comment) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/comments/${comment}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application.json',
+    },
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  createComment, getAllComments, getSingleComment, updateComment, deleteComment,
+};
